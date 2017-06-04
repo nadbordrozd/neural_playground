d93682f0781b97db149acf9c30f942021851073c
nadbor <nadbordrozd@gmail.com>
2017-06-04 13:54:50 +0100

    cleanup

--- char_rnn_tagging/get_data.sh
+++ char_rnn_tagging/get_data.sh
@@ -4,5 +4,8 @@ cd data
 wget http://www.gutenberg.org/ebooks/31100.txt.utf-8
 mv 31100.txt.utf-8 austen/austen.txt
 
+wget http://www.gutenberg.org/ebooks/100.txt.utf-8
+mv 100.txt.utf-8 shakespeare/shakespeare.txt
+
 git clone https://github.com/scikit-learn/scikit-learn.git
 git clone https://github.com/scalaz/scalaz.git
--- char_rnn_tagging/run_all.sh
+++ char_rnn_tagging/run_all.sh
@@ -3,6 +3,7 @@ bash get_data.sh
 
 python prepare_input_files.py data/austen 'austen.txt' data/austen_clean
 awk '{ print $0 > "data/austen_clean/part"++i".txt" }' RS='\n\n\n' data/austen_clean/austen.txt
+
 rm data/austen_clean/austen.txt
 python prepare_input_files.py data/scikit-learn/ '*.py' data/sklearn_clean
 
